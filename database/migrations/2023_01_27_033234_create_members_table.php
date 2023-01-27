79d0a81bef5e2e5e0ca58d418bd45efe97a9d74e
volixizes <volixizes.mickey2003@gmail.com>
2023-01-27 12:23:35 +0800

    tables migrated

--- database/migrations/2023_01_27_033234_create_members_table.php
+++ database/migrations/2023_01_27_033234_create_members_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('membership_type');
+            $table->date('membership_expiration');
             $table->timestamps();
         });
     }
