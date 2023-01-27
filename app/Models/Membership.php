246fae4327f37391ffaa9d9c0bcf0c8a6c18eec7
volixizes <volixizes.mickey2003@gmail.com>
2023-01-27 17:56:58 +0800

    Membership fillable defined

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Membership extends Model
 {
     use HasFactory;
+    protected $table = 'members';
+
+    protected $fillable = [
+        'membership_type',
+        'membership_price'
+    ]
 }
