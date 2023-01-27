914da682724906c2cf1b46ee6dbf65a1755948d7
volixizes <volixizes.mickey2003@gmail.com>
2023-01-27 13:05:20 +0800

    Route created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <form method="POST" action="#">
+                        <form method="POST" action="{{ route('createmember') }}">
                         <div class="mb-3">
                             <label for="name" class="form-label">Name</label>
                             <input type="text" class="form-control" id="name" name="name">
