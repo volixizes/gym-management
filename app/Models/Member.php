945ff40cc3db04a3c27405992dfdce7ef92e58dd
volixizes <volixizes.mickey2003@gmail.com>
2023-01-27 12:39:54 +0800

    fillable properties and relationships defined

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+
+    protected $table = 'members';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'membership_type',
+        'membership_expiration'
+    ];
 }
