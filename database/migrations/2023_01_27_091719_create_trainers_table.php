0487a9e586b405a16aba211e441a9534085a783f
volixizes <volixizes.mickey2003@gmail.com>
2023-01-27 17:34:11 +0800

    Trainer model created

--- database/migrations/2023_01_27_091719_create_trainers_table.php
+++ database/migrations/2023_01_27_091719_create_trainers_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('trainers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('specialization');
+            $table->string('phone');
             $table->timestamps();
         });
     }
