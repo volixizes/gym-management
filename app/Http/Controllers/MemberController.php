397caf434a42a0e66cb0bcc98f1246ee0748e80c
volixizes <volixizes.mickey2003@gmail.com>
2023-01-27 13:04:07 +0800

    Function created in MemberController

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Models\Member;
 
 class MemberController extends Controller
 {
-    //
+    public function create(Request $request){
+        $member = new Member;
+        $member->name = $request->name;
+        $member->email = $request->email;
+        $member->membership_type = $request->membershiptype;
+        $member->membership_expiration = $request->membershipexpiration;
+
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,14 +63,14 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <form>
+                        <form method="POST" action="#">
                         <div class="mb-3">
                             <label for="name" class="form-label">Name</label>
                             <input type="text" class="form-control" id="name" name="name">
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="name">
+                            <input type="email" class="form-control" id="email" name="email">
                         </div>
                         <div class="mb-3">
                             <label for="membershiptype" class="form-label">Membership Type</label>
